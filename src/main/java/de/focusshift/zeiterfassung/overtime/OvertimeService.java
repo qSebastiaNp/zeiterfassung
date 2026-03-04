6829f8f73bc3e8e7e509c0870fbdf148207827ed
qSebastiaNp <sebastian.flemig@posteo.de>
2026-03-04 14:36:18 +0100

    * Fix: extreme Ladezeiten beim Laden großer Zeitbereiche behoben + Feature: Einführung einer Überstundenübersicht für Mitarbeiter

--- src/main/java/de/focusshift/zeiterfassung/overtime/OvertimeService.java
+++ src/main/java/de/focusshift/zeiterfassung/overtime/OvertimeService.java
@@ -24,4 +24,14 @@ public interface OvertimeService {
      * @return {@link OvertimeHours} for the date and user
      */
     OvertimeHours getOvertimeForDateAndUser(LocalDate date, UserLocalId userLocalId);
+    
+    /**
+     * Get overtime for multiple dates for a specific user in one batch operation
+     *
+     * @param startDate start date (inclusive)
+     * @param endDate end date (inclusive)
+     * @param userLocalId user
+     * @return map of dates to overtime hours
+     */
+    Map<LocalDate, OvertimeHours> getOvertimeForDatesAndUser(LocalDate startDate, LocalDate endDate, UserLocalId userLocalId);
 }
